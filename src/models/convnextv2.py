fbf51395061940ed583433b9a71f192b6613b9f8
reneshravi <renesh2000@gmail.com>
2025-12-12 15:16:05 -0500

    Built a convnextv2 model wrapper class

---
+++ src/models/convnextv2.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import timm
+
+class ConvNeXtV2Model(nn.Module):
+    """
+    ConvNeXt V2 model wrapper for diabetic retinopathy classification.
+    """
+    
+    def __init__(self, model_name='convnextv2_tiny', num_classes=5, pretrained=True):
+        """
+        Args:
+            model_name: name of ConvNeXt V2 variant
+            num_classes: number of output classes
+            pretrained: whether to use pretrained weights
+        """
+        super(ConvNeXtV2Model, self).__init__()
+        
+        self.model_name = model_name
+        self.num_classes = num_classes
+        self.pretrained = pretrained
+        
+        # Create model
+        self.model = timm.create_model(
+            model_name,
+            pretrained=pretrained,
+            num_classes=num_classes
+        )
+        
+        print(f"Created {model_name} with {self.count_parameters():,} parameters")
+        if pretrained:
+            print("Using ImageNet pretrained weights")
+        else:
+            print("Training from scratch (random initialization)")
+    
+    def forward(self, x):
+        return self.model(x)
+    
+    def count_parameters(self):
+        """Count total trainable parameters"""
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+    
+    def freeze_backbone(self):
+        """Freeze all layers except the classifier head"""
+        for name, param in self.model.named_parameters():
+            if 'head' not in name:  # Don't freeze the final classification layer
+                param.requires_grad = False
+        
+        print("Backbone frozen. Only training classification head.")
+    
+    def unfreeze_all(self):
+        """Unfreeze all layers"""
+        for param in self.parameters():
+            param.requires_grad = True
+        
+        print("All layers unfrozen.")
+
+
+def get_model(model_name='convnextv2_tiny', num_classes=5, pretrained=True):
+    """
+    Factory function to create ConvNeXt V2 model.
+    
+    Available models:
+    - convnextv2_atto
+    - convnextv2_femto
+    - convnextv2_pico
+    - convnextv2_nano
+    - convnextv2_tiny (recommended)
+    - convnextv2_base
+    - convnextv2_large
+    
+    Args:
+        model_name: name of model variant
+        num_classes: number of output classes
+        pretrained: whether to use pretrained weights
+    
+    Returns:
+        ConvNeXtV2Model instance
+    """
+    model = ConvNeXtV2Model(
+        model_name=model_name,
+        num_classes=num_classes,
+        pretrained=pretrained
+    )
+    
+    return model
